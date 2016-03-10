227b2fc2365ff9a88a9f3f33cfee0df43c1807cb
LoganCreaser <logan.creaser@mckesson.com>
2016-03-10 13:05:52 -0500

    Create plugin-dev.js

---
+++ plugin-dev.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+videojs.plugin('pluginDev', function() {
+  var player = this,
+  overlay = document.createElement('p');
+  overlay.className = 'vjs-overlay';
+  overlay.innerHTML = "Becoming a plugin developer";
+  player.el().appendChild(overlay);
+});
